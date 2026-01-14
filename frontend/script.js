83283c9b9cc370914d9fe910a2cf5e4455ad213c
pattanshetty99 <pattanshetty99@gmail.com>
2026-01-14 14:57:52 +0530

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+function submitForm() {
+
+  const image = document.getElementById("image").files[0];
+  if (!image) {
+    alert("Please upload image");
+    return;
+  }
+
+  let answers = [];
+
+  document.querySelectorAll("input[type=checkbox]:checked")
+    .forEach(cb => answers.push(cb.value));
+
+  document.querySelectorAll("input[type=radio]:checked")
+    .forEach(rb => answers.push(rb.value));
+
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("answers", JSON.stringify(answers));
+
+  fetch("YOUR_BACKEND_URL/predict", {
+    method: "POST",
+    body: formData
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("result").innerHTML =
+      `Prakriti: <b>${data.prakriti}</b><br>
+       Confidence: ${data.confidence}`;
+  });
+}
