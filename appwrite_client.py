7e5543f2fc778739e1f5e23624ffdab3932f5c10
pattanshetty99 <pattanshetty99@gmail.com>
2026-01-16 16:25:12 +0530

    Update appwrite_client.py

--- appwrite_client.py
+++ appwrite_client.py
@@ -1,15 +1,17 @@
+import os
 from appwrite.client import Client
 from appwrite.services.databases import Databases
 from appwrite.services.storage import Storage
 
 client = Client()
-client.set_endpoint("APPWRITE_ENDPOINT")
-client.set_project("APPWRITE_PROJECT_ID")
-client.set_key("APPWRITE_API_KEY")
+
+client.set_endpoint(os.environ.get("APPWRITE_ENDPOINT"))
+client.set_project(os.environ.get("APPWRITE_PROJECT_ID"))
+client.set_key(os.environ.get("APPWRITE_API_KEY"))
 
 database = Databases(client)
 storage = Storage(client)
 
-DATABASE_ID = "DATABASE_ID"
-COLLECTION_ID = "COLLECTION_ID"
-BUCKET_ID = "BUCKET_ID"
+DATABASE_ID = os.environ.get("DATABASE_ID")
+COLLECTION_ID = os.environ.get("COLLECTION_ID")
+BUCKET_ID = os.environ.get("BUCKET_ID")
